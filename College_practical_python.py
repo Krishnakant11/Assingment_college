d5f5c50a41eec47b7ad46649901b2f5b97b3c35e
kr1shnakant9 <69688638+Krishnakant11@users.noreply.github.com>
2022-03-21 22:26:58 +0530

    Assingment

---
+++ College_practical_python.py
@@ -0,0 +1,8 @@
+import practical_of_python
+from practical_of_python import practical_10
+
+student = practical_10("Krishnakant", "IT", "Jec")
+
+print(student.college)
+
+print(practical_of_python.practical_9("krishnakant"))
